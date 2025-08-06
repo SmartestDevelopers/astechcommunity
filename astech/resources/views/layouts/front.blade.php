6ebddb3a6d12dd062a037993059eec88f3ffae43
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-08-06 18:02:42 +0500

    made frontcontroller

--- astech/resources/views/layouts/front.blade.php
+++ astech/resources/views/layouts/front.blade.php
@@ -1110,8 +1110,8 @@
 
   <!-- JavaScript -->
   <script src="../../../unpkg.com/leaflet%401.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-  <script src="js/vendors.js"></script>
-  <script src="js/main.js"></script>
+  <script src="{{ asset('astech/template/js/vendors.js' }}"></script>
+  <script src="{{ asset('astech/template/js/main.js' }}"></script>
 </body>
 
 
